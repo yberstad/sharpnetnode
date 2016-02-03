8b5b0d7003527a8ccfd266b8cc51c882bfa595b5
Oyvind Habberstad <oyvindhabberstad@gmail.com>
2016-02-03 21:24:18 +0100

    no message

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 import express from 'express';
-/*import schema from './data/schema';
-import GraphQLHTTP from 'express-qraphql';
-
-
-app.use(express.static('public'));
-app.use('/graphql', GraphQLHTTP({
-	schema,
-	graphql: true
-}))*/
+import schema from './data/schema';
+import GraphQLHTTP from 'express-graphql';
 
 let app = express();
-app.get('/', (req, res) => res.send('Hello'));
+
+app.use('/', GraphQLHTTP({
+	schema,
+	graphiql: true
+}));
+
 app.listen('3000', () => console.log('Listening on port 3000'));
